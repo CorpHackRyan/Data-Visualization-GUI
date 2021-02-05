7b1dd5c7ece2d59cd1ea2ec022ff6540722a15e4
Ryan OConnor <ryanoconnor010@gmail.com>
2021-02-05 11:51:50 -0500

    created the secrets.py and added it to .gitignore

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+import secrets
+
+
+def get_data(url: str):
+    final_data = []
+    final_url = f"{url}&api_key={secrets.api_key}"
+    response = requests.get(final_url)
+
+    if response.status_code != 200:
+        print(response.text)
+        return[]
+    json_data = response.json()
+    return final_data
+
+
+def main():
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.name,2013.student.size"
+    all_data = get_data(url)
+    print(all_data)
+
+
+if __name__ == '__main__':
+    main()
+
+
