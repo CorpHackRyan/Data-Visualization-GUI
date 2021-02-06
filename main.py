2b2ba3c70ad4ef7110ff07367275a746789ae036
Ryan OConnor <ryanoconnor010@gmail.com>
2021-02-06 12:38:18 -0500

    deleted per_page=50, taking to long
    add variables to get total results, current page and the results per page.

--- main.py
+++ main.py
@@ -11,15 +11,21 @@ def get_data(url: str):
         print(response.text)
         return[]
     json_data = response.json()
-    each_page_data = json_data["results"]
+
+    total_results = json_data["metadata"]["total"]
+    current_page = json_data["metadata"]["page"]
+    results_per_page = json_data["metadata"]["per_page"]
+
+    print(total_results,current_page, results_per_page)
 
 
-    final_data.extend(each_page_data)
+    # each_page_data = json_data["results"]
+    # final_data.extend(each_page_data)
     return final_data
 
 
 def main():
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&per_page=50&fields=school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
     all_data = get_data(url)
     for school_data in all_data:
         print(school_data)
