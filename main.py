cad64a9acdb37f9b377789621af9867095c73d70
Ryan OConnor <ryanoconnor010@gmail.com>
2021-02-06 11:44:42 -0500

    updated 'url' to include proper fields to retrieve

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import secrets
 
 def get_data(url: str):
     final_data = []
-    final_url = f"{url}&api_key={secrets.api_key}&page=9"
+    final_url = f"{url}&api_key={secrets.api_key}&page=0"
     response = requests.get(final_url)
 
     if response.status_code != 200:
@@ -17,14 +17,24 @@ def get_data(url: str):
 
 
 def main():
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.name,2013.student.size"
+    # url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.name,2013.student.size"
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
+
     all_data = get_data(url)
     for school_data in all_data:
         print(school_data)
 
 
-
 if __name__ == '__main__':
     main()
+
+
+# field names that we want to get are:
+# school.name
+# school.city
+# 2018.student.size
+# 2017.student.size
+# 2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line
+# 2016.repayment.3_yr_repayment.overall
 
 
