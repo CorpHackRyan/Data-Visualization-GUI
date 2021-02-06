ae115fa32cd3f7727a896757f1c1d3a986da7944
Ryan OConnor <ryanoconnor010@gmail.com>
2021-02-06 12:02:07 -0500

    added per_page=50 parameter to url,

--- main.py
+++ main.py
@@ -11,15 +11,15 @@ def get_data(url: str):
         print(response.text)
         return[]
     json_data = response.json()
-    page_data = json_data["results"]
-    final_data.extend(page_data)
+    each_page_data = json_data["results"]
+
+
+    final_data.extend(each_page_data)
     return final_data
 
 
 def main():
-    # url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.name,2013.student.size"
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
-
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&per_page=50&fields=school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
     all_data = get_data(url)
     for school_data in all_data:
         print(school_data)
