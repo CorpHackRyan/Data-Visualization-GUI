4c4137d961bcca3060765ad651b2bac68ed9f3c3
Ryan OConnor <ryanoconnor010@gmail.com>
2021-02-16 22:52:40 -0500

    wrote first function to test get_meta_data() function from main in test_sprint2.py

--- tests/test_sprint2.py
+++ tests/test_sprint2.py
@@ -1,16 +1,17 @@
 import pytest
 import main
 
-# tests all begin with: test_?? where ?? is the fucnction in the initial file
 
-# def test_get_meta_deta():
- #   results = some_functino(params)
+def test_get_meta_data():
+    testurl = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id," \
+          "school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_" \
+          "count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
 
-#    assert results == SOME ANSWER
+    results = main.get_metadata(testurl)
+    assert results[0] > 1000
 
 
-# to accept an error, do:
-#with pytest.raises(ValueError)
-#    newbal2=addinterest(1000,-.1) # (if theres an exception, that's a good thing,)
-#    assert newbalnce == 100
+
+#    2. The second test should create a new empty database, run your table creation function/method, then run your save data to
+#    database method then check to see that the database contains the test university that you just put there.
 
