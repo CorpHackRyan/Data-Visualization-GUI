889528266a439e8779862e4c77a7c5c47c6a92f9
Ryan OConnor <ryanoconnor010@gmail.com>
2021-02-24 20:44:06 -0500

    assertion statement passes test 1

--- tests/test_sprint.py
+++ tests/test_sprint.py
@@ -60,20 +60,10 @@ def test_xlsx_read():
 
     # Make sure our read from excel file works properly #
 
-    # So read  use the function from main to read the excel. it will then create the xcel file
-    # then, tak ea row of data of that file, and compare it to the data we have stored in our dict
-
     # Random row of data with columns we need from actual excel file
-    test_datadict = {
-        # col 1=area_title,  col7=occ_code,  col8=occ_title, col9=o_group, col10=tot_emp,  col19=h_pct25,  col24=a_pct25
-        "area_title": "Massachusetts",
-        "occ_code": "15-0000",
-        "occ_title": "Computer and Mathematical Occupations",
-        "o_group": "major",
-        "tot_emp": "147070",
-        "h_pct25": "34.25",
-        "a_pct25": "156450"
-    }
+    test_assertion_data = "Massachusetts"
+    counter = 0
+    times_assertion_data_appears = 22
 
     test_dbname = "test_db.db"
     test_xls_filename = "state_M2019_dl.xlsx"
@@ -88,28 +78,13 @@ def test_xlsx_read():
     test_result = cursor.execute("""
                                SELECT *
                                FROM jobdata_by_state
-                               WHERE area_title = ?""", (test_datadict["area_title"],))
+                               WHERE area_title = ?""", (test_assertion_data,))
 
     for row in test_result:
         print(*row)
-    # test_school_id = test_datadict["school_id"]
-    # test_meta_data = main.get_metadata(test_url)
-    # main.process_data(test_url, test_meta_data, cursor)
+        counter += 1
 
-    # for row in test_result:
-    #    print(f'School id: {row[0]}  \nTest DB school id: {test_datadict["school_id"]} \n\n'
-    #          f'School name: {row[1]}\nTest DB school name: {test_datadict["school_name"]}\n\n'
-    #          f'School city: {row[2]}\nTest DB school city: {test_datadict["school_city"]}\n\n'
-    #         f'School student size 2018: {row[3]}\nTest DB School size 2018: {test_datadict["student_size_2018"]}\n\n'
-    #          f'School student size 2017: {row[4]}\nTest DB Student size 2017: {test_datadict["student_size_2017"]}\n\n'
-    #          f'2017 earnings over poverty: {row[5]}\nTest DB 2017 earnings over poverty: '
-    #          f'{test_datadict["earnings_3_yrs_after_completion_overall_count_over_poverty_line_2017"]}\n\n'
-    #          f'3 yr repayment 2016: {row[6]}\n'
-    #          f'Test DB 3 yr repayment 2016: {test_datadict["repayment_3_yr_repayment_overall_2016"]}')
-
-    # assert row[0] == int(test_datadict["school_id"])
+    # Original excel document contains 22 'Massachusetts' entries
+    assert counter == times_assertion_data_appears
 
     main.close_db(conn)
-
-
-
