d7dfb11b0fe6e3c6d66405978cf6c014f2c8489e
Ryan OConnor <ryanoconnor010@gmail.com>
2021-03-06 18:46:04 -0500

    added GUI_Sprint4.py for GUI portion of sprint4 added pyside6 to requirements.txt

---
+++ GUI_Sprint4.py
@@ -0,0 +1,23 @@
+from PySide6.QtWidgets import QWidget, QPushButton, QListWidget, QApplication, QListWidgetItem
+from typing import List, Dict
+
+
+class Comp490DemoWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.data = 0
+        self.list_control = None
+        self.setup_window()
+
+    def setup_window(self):
+        self.setWindowTitle(" Project1 - Sprint 4 - Ryan OConnor")
+        # display_list = QListWidget(self)
+        # self.list_control = display_list
+        # self.put_data_in_list(self.data)
+        # display_list.resize(400,350)
+        self.setGeometry(100,100, 400, 500)
+        quit_button = QPushButton("Exit", self)
+        quit_button.clicked.connect(QApplication.instance().quit)
+        quit_button.resize(quit_button.sizeHint())
+        quit_button.move(150, 400)
+        self.show()
