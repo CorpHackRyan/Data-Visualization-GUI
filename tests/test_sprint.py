1606d5b9d45fd39a69ce97a7e8d3053944bf3df1
Ryan OConnor <ryanoconnor010@gmail.com>
2021-03-09 22:34:52 -0500

    cleaned up test_sprint.py

--- tests/test_sprint.py
+++ tests/test_sprint.py
@@ -14,10 +14,6 @@ def test_get_meta_data():
 
 def test_database():
     test_url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id," \
-              "school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_" \
-              "count_over_poverty_line,2016.repayment.3_yr_repayment.overall"
-
-    test_url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id," \
                "school.name,school.city,2018.student.size,2017.student.size,2017.earnings.3_yrs_after_completion.overall_" \
                "count_over_poverty_line,2016.repayment.3_yr_repayment.overall,school.state,2016.repayment.repayment_cohort.3_" \
                "year_declining_balance"
